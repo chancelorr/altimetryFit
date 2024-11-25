737e05df1ffde5c2129e6ddcb9adea07612335c4
chancelorr <ccroberts@ucsd.edu>
2024-11-25 15:44:15 -0800

    get pgc_filter.sh working

--- scripts/pgc_filter.sh
+++ scripts/pgc_filter.sh
@@ -6,7 +6,7 @@
 # Arguments
 
 # PGC s3 url
-url=$1
+url=$(echo "$1" | sed 's|https://[^/]*/stac-browser/#/external/|https://|g')
 
 # where to put the filtered file
 output_dir=$2
@@ -15,26 +15,29 @@ output_dir=$2
 # in default_args folder
 filter_arg_file=$3
 
-dem_url=`echo $url | sed s/.json/_dem.tif/`
-match_url=`echo $url | sed s/.json/_matchtag.tif/`
-mask_url=`echo $url | sed s/.json/_bitmask.tif/`
-dem_file=`basename $dem_url`
-filt_file=$(echo $dem_file | sed s/_2m_/_32m_/ | sed s/_dem.tif/_dem_filt.tif/)
+dem_url=$(sed 's/.json/_dem.tif/' <<< "$url")
+match_url=$(sed 's/.json/_matchtag.tif/' <<< "$url")
+mask_url=$(sed 's/.json/_bitmask.tif/' <<< "$url")
+dem_file=$(basename "$dem_url")
+filt_file=$(sed -e 's/_2m_/_32m_/' -e 's/_dem.tif/_dem_filt.tif/' <<< "$dem_file")
 
-echo "dem_url="$dem_url
-echo "dem_file="$dem_file
-echo "filt_file="$filt_file
+#echo "dem_url="$dem_url
+#echo "dem_file="$dem_file
+#echo "filt_file="$filt_file
 
 match_file=$(basename $match_url)
 mask_file=$(basename $mask_url)
 
 for url in $dem_url $match_url $mask_url; do
     file=$(basename $url)
-    [ -f $file ] || echo 'downloading '$dem_file | wget -q -P $output_dir $url
+    #echo "checking for "$output_dir"/"$file""
+    [ -f "$output_dir"/"$file" ] || echo "downloading $file"
+    [ -f "$output_dir"/"$file" ] || wget -q --show-progress -P $output_dir $url
 done
 
 #wget -q $dem_url && wget -q $match_url && wget -q $mask_url || $(echo "COULD NOT DOWNLOAD"; exit 1)
 
+echo -e "\nrunning dem_filter.py\n"
 [ -f $filter_arg_file ] && dem_filter.py $dem_file $filt_file @$filter_arg_file && exit
 
 [ -f default_args.txt ] && dem_filter.py $dem_file $filt_file @default_args.txt
